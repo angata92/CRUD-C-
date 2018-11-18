b9cdb9c4e0c8a56a798554bf86f1c859d4771488
Angel Dimanov <ang_el_@abv.bg>
2018-11-18 14:46:34 +0200

    Add files via upload

---
+++ Backup/CRUDCSHARPSQLSERVER/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace CRUDCSHARPSQLSERVER
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtuname = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cborole = new System.Windows.Forms.ComboBox();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.dtgList = new System.Windows.Forms.DataGridView();
+            this.btndel = new System.Windows.Forms.Button();
+            this.btnreload = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.btnnew = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(112, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User CRUD";
+            // 
+            // txtname
+            // 
+            this.txtname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtname.Location = new System.Drawing.Point(96, 78);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(248, 26);
+            this.txtname.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(38, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Username:";
+            // 
+            // txtuname
+            // 
+            this.txtuname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtuname.Location = new System.Drawing.Point(96, 110);
+            this.txtuname.Name = "txtuname";
+            this.txtuname.Size = new System.Drawing.Size(248, 26);
+            this.txtuname.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(11, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Password:";
+            // 
+            // txtpass
+            // 
+            this.txtpass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpass.Location = new System.Drawing.Point(96, 142);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(248, 26);
+            this.txtpass.TabIndex = 5;
+            this.txtpass.UseSystemPasswordChar = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(47, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Role:";
+            // 
+            // cborole
+            // 
+            this.cborole.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cborole.FormattingEnabled = true;
+            this.cborole.Items.AddRange(new object[] {
+            "Administrator",
+            "Staff",
+            "Encoder"});
+            this.cborole.Location = new System.Drawing.Point(96, 174);
+            this.cborole.Name = "cborole";
+            this.cborole.Size = new System.Drawing.Size(248, 28);
+            this.cborole.TabIndex = 9;
+            this.cborole.Text = "Select";
+            // 
+            // btnsave
+            // 
+            this.btnsave.AutoSize = true;
+            this.btnsave.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsave.Location = new System.Drawing.Point(350, 65);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(110, 28);
+            this.btnsave.TabIndex = 10;
+            this.btnsave.Text = "Create";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // dtgList
+            // 
+            this.dtgList.AllowUserToAddRows = false;
+            this.dtgList.AllowUserToDeleteRows = false;
+            this.dtgList.AllowUserToResizeColumns = false;
+            this.dtgList.AllowUserToResizeRows = false;
+            this.dtgList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgList.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dtgList.Location = new System.Drawing.Point(12, 217);
+            this.dtgList.Name = "dtgList";
+            this.dtgList.RowHeadersVisible = false;
+            this.dtgList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dtgList.Size = new System.Drawing.Size(452, 150);
+            this.dtgList.TabIndex = 11;
+            this.dtgList.DoubleClick += new System.EventHandler(this.dtgList_DoubleClick);
+            // 
+            // btndel
+            // 
+            this.btndel.AutoSize = true;
+            this.btndel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndel.Location = new System.Drawing.Point(350, 155);
+            this.btndel.Name = "btndel";
+            this.btndel.Size = new System.Drawing.Size(110, 28);
+            this.btndel.TabIndex = 12;
+            this.btndel.Text = "Delete";
+            this.btndel.UseVisualStyleBackColor = true;
+            this.btndel.Click += new System.EventHandler(this.btndel_Click);
+            // 
+            // btnreload
+            // 
+            this.btnreload.AutoSize = true;
+            this.btnreload.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnreload.Location = new System.Drawing.Point(350, 95);
+            this.btnreload.Name = "btnreload";
+            this.btnreload.Size = new System.Drawing.Size(110, 28);
+            this.btnreload.TabIndex = 13;
+            this.btnreload.Text = "Reload";
+            this.btnreload.UseVisualStyleBackColor = true;
+            this.btnreload.Click += new System.EventHandler(this.btnreload_Click);
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.AutoSize = true;
+            this.btnupdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnupdate.Location = new System.Drawing.Point(350, 125);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(110, 28);
+            this.btnupdate.TabIndex = 13;
+            this.btnupdate.Text = "Update";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click);
+            // 
+            // btnnew
+            // 
+            this.btnnew.AutoSize = true;
+            this.btnnew.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnnew.Location = new System.Drawing.Point(350, 183);
+            this.btnnew.Name = "btnnew";
+            this.btnnew.Size = new System.Drawing.Size(110, 28);
+            this.btnnew.TabIndex = 14;
+            this.btnnew.Text = "New";
+            this.btnnew.UseVisualStyleBackColor = true;
+            this.btnnew.Click += new System.EventHandler(this.btnnew_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 382);
+            this.Controls.Add(this.btnnew);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.btnreload);
+            this.Controls.Add(this.btndel);
+            this.Controls.Add(this.dtgList);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.cborole);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtuname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtuname;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cborole;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.DataGridView dtgList;
+        private System.Windows.Forms.Button btndel;
+        private System.Windows.Forms.Button btnreload;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Button btnnew;
+    }
+}
+
---
+++ CRUDCSHARPSQLSERVER/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CRUDCSHARPSQLSERVER
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //initialize all classes
+            SqlConnection conn = new SqlConnection();
+            SqlCommand cmd = new SqlCommand();
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataTable dt = new DataTable();
+
+        //declaring variables
+            string query;
+            int  result;
+            int userid;
+
+        //create a method for clearing all textbox
+            private void clear()
+            {
+                userid =0;
+                txtname.Clear();
+                txtuname.Clear();
+                txtpass.Clear();
+                cborole.Text = "Select"; 
+            }
+         
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //set a connection between SQL server and Visual C#
+            conn.ConnectionString = "Data Source=.\\SQLEXPRESS;database=myFirstCruddb;trusted_connection=true;";
+           //AppDomain.CurrentDomain.SetData("DataDirectory", Application.StartupPath + "\\");
+
+            //conn.ConnectionString = "Data Source=.\\SQLEXPRESS;" +
+            //"User Instance=true;" +
+            //"Integrated Security=true;" +
+            //"AttachDbFilename=" + Application.StartupPath + "\\myFirstCruddb.mdf;";
+
+        }
+
+        //Creating Method
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //opening connection
+                conn.Open();
+                //create an insert query;
+                query = "INSERT INTO tblcrud (NAME,UNAME,PASS,UTYPE) VALUES('" + txtname.Text + "','" + txtuname.Text + "','" + txtpass.Text + "','" + cborole.Text + "')";
+                //it holds the data to be executed.
+                cmd.Connection = conn;
+                cmd.CommandText = query;
+                //execute the data.
+                result = cmd.ExecuteNonQuery();
+                //validate if the result of the executed query.
+                if (result > 0)
+                {
+                    
+                    MessageBox.Show("Data has been saved in the SQL database");
+                    //calling a method
+                    btnreload_Click(sender, e);
+
+                }
+                else
+                {
+                    MessageBox.Show("SQL QUERY ERROR");
+                }
+                //closing connection
+                conn.Close();
+            
+            }
+            catch(Exception ex)//catch exeption
+            {
+                //displaying error message.
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        //delete method
+        private void btndel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                conn.Open();
+                //delete query
+                query = "DELETE FROM tblcrud WHERE ID = " + dtgList.CurrentRow.Cells["ID"].FormattedValue;
+                //it holds the data to be executed.
+                cmd.Connection = conn;
+                cmd.CommandText = query;
+                //execute the data.
+                result = cmd.ExecuteNonQuery();
+                //validate if the result of the executed query.
+
+                if (result > 0)
+                {
+                    MessageBox.Show("Data has been deleted in the SQL database");
+                    //calling a method
+                    btnreload_Click(sender, e);
+                }
+                else
+                {
+                    MessageBox.Show("SQL QUERY ERROR");
+                }
+                
+            }
+          
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message ); 
+            }
+            finally
+            { 
+                conn.Close();
+            }
+
+        }
+        //diplay the specific data from the datagridview to the textbox 
+        private void dtgList_DoubleClick(object sender, EventArgs e)
+        {
+            userid =  Int32.Parse(dtgList.CurrentRow.Cells["Id"].FormattedValue.ToString());
+            txtname.Text = dtgList.CurrentRow.Cells["Name"].FormattedValue.ToString();
+            txtuname.Text = dtgList.CurrentRow.Cells["Username"].FormattedValue.ToString();
+            txtpass.Text = dtgList.CurrentRow.Cells["PASS"].FormattedValue.ToString();
+            cborole.Text = dtgList.CurrentRow.Cells["Role"].FormattedValue.ToString();       
+        }
+
+        //load method
+        private void btnreload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               //create a query for retrieving data in the database.
+                query = "SELECT ID as 'Id',NAME as 'Name',UNAME as 'Username', UTYPE as 'Role',PASS FROM tblcrud";
+                //initialize new Sql commands
+                cmd = new SqlCommand();
+                //hold the data to be executed.
+                cmd.Connection = conn;
+                cmd.CommandText = query;
+                //initialize new Sql data adapter
+                da = new SqlDataAdapter();
+                //fetching query in the database.
+                da.SelectCommand = cmd;
+                //initialize new datatable
+                dt = new DataTable();
+                //refreshes the rows in specified range in the datasource. 
+                da.Fill(dt);
+                //set the data that to be display in the datagridview
+                dtgList.DataSource = dt;
+                //hide the password column in the datagridview 
+                dtgList.Columns["PASS"].Visible  = false;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                da.Dispose();
+                 
+            }
+            userid = 0;
+
+        }
+        //update method
+        private void btnupdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                query = "UPDATE tblcrud SET NAME='" + txtname.Text + "',UNAME='" + txtuname.Text + "',PASS='" + txtpass.Text + "' ,UTYPE='" + cborole.Text + "' WHERE ID = " + userid;
+                cmd.Connection = conn;
+                cmd.CommandText = query;
+                result = cmd.ExecuteNonQuery();
+
+                if (result > 0)
+                {
+                    MessageBox.Show("Data has been updated in the SQL database");
+                    //calling a method
+                    btnreload_Click(sender, e);
+                }
+                else
+                {
+                    MessageBox.Show("SQL QUERY ERROR");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        //new method
+        private void btnnew_Click(object sender, EventArgs e)
+        {
+            //calling a method
+            clear();
+        }
+
+     
+        
+    }
+}
